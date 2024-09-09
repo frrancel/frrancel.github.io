b756523ed1e256d9f52299abcc85e22eddb8239f
frrancel <ramosfrancelvan@yahoo.com>
2024-09-09 03:20:45 +0100

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ document.addEventListener("DOMContentLoaded", () => {
     for(const image of images) {
         fetch ("https://dog.ceo/api/breeds/image/random")
         .then(response => response.json())
-        .then(data => image.src = data.message)
+        .then(data => {
+            image.src = data.message
+            image.width = 100;
+            image.height = 100;
+        })
     }
-
 })
